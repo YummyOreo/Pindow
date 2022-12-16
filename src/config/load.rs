e8647b6f25643aab3161ad7deadb79b906f13527
YummyOreo <bobgim20@gmail.com>
2022-12-15 18:58:02 -0600

    starts with the migration

--- src/config/load.rs
+++ src/config/load.rs
@@ -1,6 +1,7 @@
 use device_query::Keycode;
 
 use crate::config::options;
+use crate::config::key;
 
 pub fn load_string(file: String) -> String {
     match std::fs::read_to_string(&file) {
@@ -19,181 +20,85 @@ pub fn make_file(file: &String) {
     std::fs::write(file, "{\"configs\": [{}]}").expect("Unable to write file");
 }
 
-fn map_keybindings(keybindings_str: options::KeybindingsStr) -> options::Keybindings {
-    let mut debug_close: Vec<Keycode>;
-    match keybindings_str.debug_close {
-        None => {
-            debug_close = vec![Keycode::RControl, Keycode::RAlt];
-        }
-        Some(s) => {
-            debug_close = Vec::new();
-            for key in s {
-                debug_close.insert(0, keycode_from_string(&key).unwrap());
-            }
-        }
-    }
+fn map_keymaps(maps: Option<options::KeybindingsStr>) -> Vec<key::Keybind> {
+    vec![]
+}
 
-    let mut app_num: Vec<Keycode>;
-    match keybindings_str.app_num {
-        None => {
-            app_num = vec![Keycode::LControl, Keycode::Comma];
-        }
-        Some(s) => {
-            app_num = Vec::new();
-            for key in s {
-                app_num.insert(0, keycode_from_string(&key).unwrap());
+fn map_app_commands(app_commands_str: Option<Vec<options::AppCommandStr>>) -> Vec<options::AppCommand> {
+    match app_commands_str {
+        Some(apps) => {
+            let mut app_commands = vec![];
+            for i in apps {
+                app_commands.push(options::AppCommand{
+                    app: i.app_path,
+                    args: i.args.unwrap_or(vec![])
+                });
             }
-        }
-    }
-
-    let mut add_app: Vec<Keycode>;
-    match keybindings_str.add_app {
-        None => {
-            add_app = vec![Keycode::LControl, Keycode::Apostrophe];
-        }
-        Some(s) => {
-            add_app = Vec::new();
-            for key in s {
-                add_app.insert(0, keycode_from_string(&key).unwrap());
-            }
-        }
-    }
-
-    let mut change_config: Vec<Keycode>;
-    match keybindings_str.change_config {
-        None => {
-            change_config = vec![Keycode::LControl, Keycode::Grave];
-        }
-        Some(s) => {
-            change_config = Vec::new();
-            for key in s {
-                change_config.insert(0, keycode_from_string(&key).unwrap());
-            }
-        }
-    };
-
-    options::Keybindings {
-        app_num,
-        add_app,
-        change_config,
-        debug_close,
+            app_commands
+        },
+        None => vec![]
     }
 }
 
-pub fn map_config(options: options::ConfigStr, index: usize) -> options::Config {
-    let keybindings: options::KeybindingsStr;
-    match options.keybindings {
-        None => {
-            keybindings = options::KeybindingsStr {
-                app_num: None,
-                add_app: None,
-                change_config: None,
-                debug_close: None,
-            };
-        }
-        Some(s) => {
-            keybindings = s;
-        }
-    }
-
-    let mut app_commands: Vec<options::AppCommand>;
-    match options.apps {
-        None => {
-            app_commands = Default::default();
-        }
-        Some(s) => {
-            let app_commands_str = s.to_vec();
-            app_commands = Vec::new();
-            for command in app_commands_str.into_iter() {
-                let mut app_command: options::AppCommand = Default::default();
-                app_command.app = command.app_path;
-                if let Some(args) = command.args {
-                    app_command.args = args;
-                }
-                app_commands.insert(app_commands.len(), app_command);
-            }
-        }
-    }
-
-    let timeout: u128;
-    match options.timeout {
-        None => {
-            timeout = 5 * 1000;
-        }
-        Some(s) => {
-            timeout = s * 1000;
-        }
-    }
-
-    let name: String;
-    match options.name {
-        None => {
-            name = index.to_string();
-        }
-        Some(s) => {
-            name = s;
-        }
-    }
-
+fn map_config(config_str: options::ConfigStr, index: i32) -> options::Config {
     options::Config {
-        name,
-        app_commands,
-        timeout,
-        key_bindings: map_keybindings(keybindings),
+        name: config_str.name.unwrap_or(index.to_string()),
+        app_commands: map_app_commands(config_str.apps),
+        timeout: config_str.timeout.unwrap_or(5),
+        keymaps: map_keymaps(config_str.keymaps),
     }
 }
 
-pub fn map_configurations(config: options::ConfigurationsStr) -> options::Configurations {
-    let mut configs: Vec<options::Config> = vec![];
-    for i in config.configs {
-        configs.insert(configs.len(), map_config(i, configs.len()));
-    }
-    options::Configurations {
+pub fn map_options(config_str: options::OptionsStr) -> options::Options {
+    let mut configs: Vec<options::Config> = Vec::default();
+    let options = options::Options {
         configs,
         current_config: 0,
-        args: options::Args {
-            ..Default::default()
-        },
+        args: Default::default(),
+    };
+
+    for i in config_str.configs {
     }
+    options
 }
 
 fn keycode_from_string(s: &str) -> Option<Keycode> {
     match s {
-        "Key1" => Some(Keycode::Key1),
-        "Key2" => Some(Keycode::Key2),
-        "Key3" => Some(Keycode::Key3),
-        "Key4" => Some(Keycode::Key4),
-        "Key5" => Some(Keycode::Key5),
-        "Key6" => Some(Keycode::Key6),
-        "Key7" => Some(Keycode::Key7),
-        "Key8" => Some(Keycode::Key8),
-        "Key9" => Some(Keycode::Key9),
-        "A" => Some(Keycode::A),
-        "B" => Some(Keycode::B),
-        "C" => Some(Keycode::C),
-        "D" => Some(Keycode::D),
-        "E" => Some(Keycode::E),
-        "F" => Some(Keycode::F),
-        "G" => Some(Keycode::G),
-        "H" => Some(Keycode::H),
-        "I" => Some(Keycode::I),
-        "J" => Some(Keycode::J),
-        "K" => Some(Keycode::K),
-        "L" => Some(Keycode::L),
-        "M" => Some(Keycode::M),
-        "N" => Some(Keycode::N),
-        "O" => Some(Keycode::O),
-        "P" => Some(Keycode::P),
-        "Q" => Some(Keycode::Q),
-        "R" => Some(Keycode::R),
-        "S" => Some(Keycode::S),
-        "T" => Some(Keycode::T),
-        "U" => Some(Keycode::U),
-        "V" => Some(Keycode::V),
-        "W" => Some(Keycode::W),
-        "X" => Some(Keycode::X),
-        "Y" => Some(Keycode::Y),
-        "Z" => Some(Keycode::Z),
+        "1" => Some(Keycode::Key1),
+        "2" => Some(Keycode::Key2),
+        "3" => Some(Keycode::Key3),
+        "4" => Some(Keycode::Key4),
+        "5" => Some(Keycode::Key5),
+        "6" => Some(Keycode::Key6),
+        "7" => Some(Keycode::Key7),
+        "8" => Some(Keycode::Key8),
+        "9" => Some(Keycode::Key9),
+        "a" => Some(Keycode::A),
+        "b" => Some(Keycode::B),
+        "c" => Some(Keycode::C),
+        "d" => Some(Keycode::D),
+        "e" => Some(Keycode::E),
+        "f" => Some(Keycode::F),
+        "g" => Some(Keycode::G),
+        "h" => Some(Keycode::H),
+        "i" => Some(Keycode::I),
+        "j" => Some(Keycode::J),
+        "k" => Some(Keycode::K),
+        "l" => Some(Keycode::L),
+        "m" => Some(Keycode::M),
+        "n" => Some(Keycode::N),
+        "o" => Some(Keycode::O),
+        "p" => Some(Keycode::P),
+        "q" => Some(Keycode::Q),
+        "r" => Some(Keycode::R),
+        "s" => Some(Keycode::S),
+        "t" => Some(Keycode::T),
+        "u" => Some(Keycode::U),
+        "v" => Some(Keycode::V),
+        "w" => Some(Keycode::W),
+        "x" => Some(Keycode::X),
+        "y" => Some(Keycode::Y),
+        "z" => Some(Keycode::Z),
         "F1" => Some(Keycode::F1),
         "F2" => Some(Keycode::F2),
         "F3" => Some(Keycode::F3),
@@ -208,8 +113,8 @@ fn keycode_from_string(s: &str) -> Option<Keycode> {
         "F12" => Some(Keycode::F12),
         "Escape" => Some(Keycode::Escape),
         "Space" => Some(Keycode::Space),
-        "LControl" => Some(Keycode::LControl),
-        "RControl" => Some(Keycode::RControl),
+        "LCtrl" => Some(Keycode::LControl),
+        "RCtrl" => Some(Keycode::RControl),
         "LShift" => Some(Keycode::LShift),
         "RShift" => Some(Keycode::RShift),
         "LAlt" => Some(Keycode::LAlt),
@@ -229,17 +134,17 @@ fn keycode_from_string(s: &str) -> Option<Keycode> {
         "PageDown" => Some(Keycode::PageDown),
         "Insert" => Some(Keycode::Insert),
         "Delete" => Some(Keycode::Delete),
-        "Grave" => Some(Keycode::Grave),
-        "Minus" => Some(Keycode::Minus),
-        "Equal" => Some(Keycode::Equal),
-        "LeftBracket" => Some(Keycode::LeftBracket),
-        "RightBracket" => Some(Keycode::RightBracket),
-        "BackSlash" => Some(Keycode::BackSlash),
-        "Semicolon" => Some(Keycode::Semicolon),
-        "Apostrophe" => Some(Keycode::Apostrophe),
-        "Comma" => Some(Keycode::Comma),
-        "Dot" => Some(Keycode::Dot),
-        "Slash" => Some(Keycode::Slash),
+        "~" => Some(Keycode::Grave),
+        "-" => Some(Keycode::Minus),
+        "=" => Some(Keycode::Equal),
+        "[" => Some(Keycode::LeftBracket),
+        "]" => Some(Keycode::RightBracket),
+        "\\" => Some(Keycode::BackSlash),
+        ";" => Some(Keycode::Semicolon),
+        "'" => Some(Keycode::Apostrophe),
+        "," => Some(Keycode::Comma),
+        "." => Some(Keycode::Dot),
+        "/" => Some(Keycode::Slash),
         _ => panic!("failed to parse keycode"),
     }
 }
